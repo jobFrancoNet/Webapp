b8841874890ceda050fb1248a6b63cb2257e6429
MattiaDeMarco <mattia.dmarco@gmail.com>
2020-06-17 20:14:11 +0200

    Aggiunta cartella di lavoro.

--- ciaomondo.py
+++ ciaomondo.py
@@ -1,14 +1,1 @@
 
-
-i""Inserire in una lista dei dati immessi in input"""
-
-attivita = input('inserisci la tipologia della tua attività: ')
-
-categoria = ['supermercato', 'farmacia']
-
-if str(attivita) == categoria:
-    print("La tua attività è già presente")
-else:
-    categoria.append(attivita)
-
-print(categoria)
